9537ba14ce5cd7d17bacf4a998489c6c804fd54f
felipefsfs <felipe.fonseca.silva@gmail.com>
2020-07-05 17:28:33 -0300

    adde writer + test

--- writer.js
+++ writer.js
@@ -1,17 +1,6 @@
 export {
-  stringy,
   write_text,
 };
-
-//remove
-function stringy(lines = [""]) {
-  if (!Array.isArray(lines)) {
-    return `${JSON.stringify(lines)}`;
-  }
-  return lines.reduce((full_srt, line) => {
-    return `${full_srt}\n${JSON.stringify(line)}`;
-  }, "");
-}
 
 async function write_text({
   encoder = new TextEncoder(),
---
+++ writer_test.js
@@ -0,0 +1,20 @@
+import {
+  assertEquals,
+} from "https://deno.land/std/testing/asserts.ts";
+import { write_text } from "./writer.js";
+
+async function setup() {
+  const file_name = "./test_file/writehere";
+  const file = await Deno.open(file_name, { read: true, append: true, create: true });
+  return file;
+}
+async function teardown(file) {
+  file.close();
+  await Deno.remove("./test_file/writehere");
+}
+Deno.test("write line", async function () {
+  const file = await setup();
+  const bytes = await write_text({file, text: "test text"});
+  assertEquals(bytes, 9);;
+  await teardown(file);
+});
