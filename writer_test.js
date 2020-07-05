b8701cdafe40034ce17d0a54e1ba32d0d4ccd8a7
felipefsfs <felipe.fonseca.silva@gmail.com>
2020-07-05 17:51:57 -0300

    fmt

--- writer_test.js
+++ writer_test.js
@@ -5,7 +5,10 @@ import { write_text } from "./writer.js";
 
 async function setup() {
   const file_name = "./test_file/writehere";
-  const file = await Deno.open(file_name, { read: true, append: true, create: true });
+  const file = await Deno.open(
+    file_name,
+    { read: true, append: true, create: true },
+  );
   return file;
 }
 async function teardown(file) {
@@ -14,7 +17,7 @@ async function teardown(file) {
 }
 Deno.test("write line", async function () {
   const file = await setup();
-  const bytes = await write_text({file, text: "test text"});
-  assertEquals(bytes, 9);;
+  const bytes = await write_text({ file, text: "test text" });
+  assertEquals(bytes, 9);
   await teardown(file);
 });
